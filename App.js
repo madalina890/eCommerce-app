14e0ec7f4acc8d3db2c3cee09f782b0895d1a356
madalina890 <87119717+madalina890@users.noreply.github.com>
2021-11-03 22:57:58 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,36 +1,35 @@
 import React from 'react';
-import Product from './components/Product';
-import data from './data';
+import {BrowserRouter, Link, Route} from 'react-router-dom';
+import CartScreen from './screens/CartScreen';
+
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="grid-container">
       <header className="row">
         <div>
-          <a href="/"><img className="logo" src="images/logo-removed.png"/></a>
+          <Link to="/"><img className="logo" src="images/logo-removed.png" alt="logo"/></Link>
         </div>
         <div>
-          <a href="/bag">Bag</a>
-          <a href="/signin">Sign In</a>
+          <Link to="/bag">Bag</Link>
+          <Link to="/signin">Sign In</Link>
         </div>
       </header>
       <main>
-        <div>
-                        <div className="row center">
-                          {
-                            data.products.map((product) => (
-                              <Product key={product.id} product={product}></Product>
-                            ))
-                          }
-                            
-                            
-                                </div>
-                            </div>
+        <Route path ="/bag/:id?" component ={CartScreen}></Route>
+        <Route path="/product/:id" component={ProductScreen}></Route>
+        <Route path="/" component={HomeScreen} exact></Route>
+        
+       
       </main>
       <footer className="row center">All right reserved</footer>
     </div>                 
 
-                        
+         </BrowserRouter>               
   );
 }
 
