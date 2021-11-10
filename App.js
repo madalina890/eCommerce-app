dad34c46b71b91420f107d151ec5b4f34b203698
madalina890 <87119717+madalina890@users.noreply.github.com>
2021-11-11 00:27:59 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,110 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { BrowserRouter, Link, Route } from "react-router-dom";
+import { signout } from "./actions/userActions";
+import CartScreen from "./screens/CartScreen";
+import HomeScreen from "./screens/HomeScreen";
+import OrderHistoryScreen from "./screens/OderHistoryScreen";
+import OrderScreen from "./screens/OrderScreen";
+import PaymentMethodScreen from "./screens/PaymentMethodScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import ProductScreen from "./screens/ProductScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import ShippingAdressScreen from "./screens/ShippingAdressScreen";
+import SigninScreen from "./screens/SigninScreen";
+import ProfileScreen from './screens/ProfileScreen';
+import PrivateRoute from './components/PrivateRoute';
+
+function App() {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  const dispatch = useDispatch();
+  const signoutHandler = () => {
+    dispatch(signout());
+  };
+  return (
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <Link to="/">
+              <img className="logo" src="images/logo-removed.png" alt="logo" />
+            </Link>
+          </div>
+          <div>
+            <Link to="/bag">
+              Bag
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            {userInfo ? (
+              <div className="dropdown">
+                <Link to="#">
+                  {userInfo.name} <i className="fa fa-caret-down"></i>{' '}
+                </Link>
+                <ul className="dropdown-content">
+                <li>
+                    <Link to="/profile">User Profile</Link>
+                  </li>
+                <li>
+                    <Link to="/orderhistory">Order History</Link>
+                  </li>
+                  <li>
+                    <Link to="#signout" onClick={signoutHandler}>
+                      Sign Out
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              <Link to="/signin">Sign In</Link>
+            )}
+            
+            {userInfo && userInfo.isAdmin && (
+              <div className="dropdown">
+                <Link to="#admin">
+                  Admin <i className="fa fa-caret-down"></i>
+                </Link>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="/dashboard">Dashboard</Link>
+                  </li>
+                  <li>
+                    <Link to="/productlist">Products</Link>
+                  </li>
+                  <li>
+                    <Link to="/orderlist">Orders</Link>
+                  </li>
+                  <li>
+                    <Link to="/userlist">Users</Link>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+        </header>
+        <main>
+          <Route path="/bag/:id?" component={CartScreen}></Route>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/signin" component={SigninScreen}></Route>
+          <Route path="/register" component={RegisterScreen}></Route>
+          <Route path="/shipping" component={ShippingAdressScreen}></Route>
+          <Route path="/payment" component={PaymentMethodScreen}></Route>
+          <Route path="/placeorder" component={PlaceOrderScreen}></Route>
+          <Route path="/order/:id" component={OrderScreen}></Route>
+          <Route path="/orderhistory" component={OrderHistoryScreen}></Route>
+          <PrivateRoute
+            path="/profile"
+            component={ProfileScreen}
+          ></PrivateRoute>
+          <Route path="/" component={HomeScreen} exact></Route>
+        </main>
+        <footer className="row center">All right reserved</footer>
+      </div>
+    </BrowserRouter>
+  );
+}
+export default App;
--- data.js
+++ data.js
@@ -1,25 +1,7 @@
-import bcrypt from'bcryptjs'
-
 const data ={
-
-    users:[
-        {
-            name:'Andreea',
-            email:'admin@themail.com',
-            password:bcrypt.hashSync('1234',8),
-            isAdmin:true,
-        },
-        {
-            name:'Alina',
-            email:'user@themail.com',
-            password:bcrypt.hashSync('1234',8),
-            isAdmin:false,
-        },
-
-    ],
     products:[
         {
-            
+            _id:'1',
             name:'Vitammin E & B5 Rose & Violet Serum',
             category:'Face',
             image:'/images/poster1.png',
@@ -27,10 +9,10 @@ const data ={
             countInStock: 7,
             rating:4.5,
             numReviews:10,
-            description:'The perfect balance for dry and mixed skin.',
+            description:'The perfect balance for dry and mixed skin',
         },
         {
-            
+            _id:'2',
             name:'Pinaple body scrub & Coconut body lotion',
             category:'Body',
             image:'/images/poster2.png',
@@ -41,7 +23,7 @@ const data ={
             description:'A blessing during the bath and after. Fall offer only.',
         },
         {
-            
+            _id:'3',
             name:'Geranium cleanser',
             category:'Face',
             image:'/images/poster3.png',
@@ -49,10 +31,10 @@ const data ={
             countInStock: 32,
             rating:4,
             numReviews:8,
-            description:'The delicate foam makes the cleansing process feel like your skin is touched by rose petals.',
+            description:'The delicate foam makes the cleaning process feel like a blessing',
         },
         {
-            
+            _id:'4',
             name:'Tea tree daytime cream & night cream. ',
             category:'Face',
             image:'/images/poster4.png',
@@ -63,7 +45,7 @@ const data ={
             description:'Top customers choice.Best selling product for skin regeneration and oil control. Limited edition only.',
         },
         {
-            
+            _id:'5',
             name:'Mint and dragonfruit mask- 500g',
             category:'Face',
             image:'/images/poster5.png',
@@ -71,10 +53,10 @@ const data ={
             countInStock: 17,
             rating:5,
             numReviews:10,
-            description:'The best formula for tired skin is no longer a screet but ready to share with the world.',
+            description:'The best formula for tired skin is no longer a screet but ready to share with the world',
         },
         {
-            
+            _id:'6',
             name:'Passionfruit & Starfruit masks to go',
             category:'Face',
             image:'/images/poster6.png',
@@ -82,10 +64,10 @@ const data ={
             countInStock: 0,
             rating:3.5,
             numReviews:6,
-            description:'Just for the right amount of fresh.',
+            description:'Just for the right amount of fresh',
         },
         {
-            
+            _id:'7',
             name:' Basil & Tiger Lilies and Retinol Eye cream',
             category:'Face',
             image:'/images/poster7.png',
@@ -93,7 +75,7 @@ const data ={
             countInStock: 10,
             rating:4.5,
             numReviews:18,
-            description:'Our doctors descovered that this combination reduces dark circles by 68%.',
+            description:'Our doctors descovered that this combination reduces dark circles by 68% ',
         },
     ],
 };
